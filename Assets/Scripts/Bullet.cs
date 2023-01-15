a6b54293219c389553c8f2885270a5206b24a964
MSI\91800 <rathodketan042@gmail.com>
2023-01-15 14:39:45 +0530

    added unityengine.pool API in shoot system

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float moveForce;
     private Vector2 direction;
     private Rigidbody2D myRd;
-    
+    public ObjectPool<Bullet> ObjectPool;
+
     private void Awake()
     {
         myRd=GetComponent<Rigidbody2D>();
@@ -16,5 +18,10 @@ public class Bullet : MonoBehaviour
     {
         direction=newDirection;
         myRd.AddForce(direction*moveForce,ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        ObjectPool.Release(this);
     }
 }
--- Assets/Scripts/Character/CharacterShoot.cs
+++ Assets/Scripts/Character/CharacterShoot.cs
@@ -2,12 +2,49 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class CharacterShoot : CharacterController
 {
     [SerializeField] Bullet bulletPrefab;
     [SerializeField] Transform gunEndPoint;
     [SerializeField] Transform gunStartPoint;
+    ObjectPool<Bullet> bulletPool;
+    private bool collectionCheck;
+    [SerializeField] private int defaultCapacity=10;
+    [SerializeField] private int maxSize=20;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    private void Awake()
+    {
+        bulletPool=new ObjectPool<Bullet>(createFunc,actionOnGet,actionOnRelease,actionOnDestroy,collectionCheck,defaultCapacity,maxSize);
+    }
+
+    private void actionOnDestroy(Bullet obj)
+    {
+        Destroy(obj.gameObject);
+    }
+
+    private void actionOnRelease(Bullet obj)
+    {
+        obj.gameObject.SetActive(false);
+    }
+
+    private void actionOnGet(Bullet obj)
+    {
+        obj.transform.position=gunEndPoint.position;
+        obj.gameObject.SetActive(true);
+    }
+
+    private Bullet createFunc()
+    {
+        var bullet=Instantiate(bulletPrefab,gunEndPoint.position,Quaternion.identity);
+        bullet.ObjectPool=bulletPool;
+        return bullet;
+    }
+
     public override void UpdateBase()
     {
         if (Input.GetMouseButtonDown(0))
@@ -18,9 +55,8 @@ public class CharacterShoot : CharacterController
 
     private void Shoot()
     {
+        bulletPool.Get(out Bullet bullet);
         var directionToShoot=(gunEndPoint.position-gunStartPoint.position).normalized;
-
-        var bullet=Instantiate(bulletPrefab,gunEndPoint.position,Quaternion.identity);
         bullet.SetBulletDirection(directionToShoot);
     }
 }
